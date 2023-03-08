71614b1f57f24b63d887ca274656d49838e830ae
ceogenbit <99147443+ceogenbit@users.noreply.github.com>
2023-03-08 12:31:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let Body = document.querySelector('body')
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+	Body.classList.toggle('body--lock')
 })
